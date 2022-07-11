e67218ef217522e13b88da057d92288752120a21
citkane <michael@openpoint.ie>
2022-07-11 16:13:40 +0100

    example docs for bug

--- packages/tools/src/mioTools.ts
+++
@@ -1,6 +0,0 @@
-
-export type tooltype = string;
-export const foo = 'mioTools';
-export const tools = () => {
-	console.log('tools');
-}
--- packages/users/src/mioUsers.ts
+++
@@ -1,5 +0,0 @@
-export type usertype = string;
-export const bar = 'mioUsers';
-export function users(){
-	console.log('users');
-}
